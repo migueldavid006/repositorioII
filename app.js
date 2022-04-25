1252e1fc6ca5f364556da4a0e87797cef21fbc41
migueldavid006 <migueldavid006@hotmail.com>
2022-04-24 21:46:49 -0500

    first commit portafolio two

---
+++ app.js
@@ -0,0 +1,101 @@
+function mostrarPremio(){
+    document.getElementById('premio').style.display = 'block';
+   document.getElementById('btn1').style.display = 'none';
+}
+
+function ocultarPremio(){
+    document.getElementById('premio').style.display = 'none';
+    document.getElementById('btn1').style.display = 'block';
+     
+}
+
+
+// function mostrarPremio(){
+//     document.getElementById('premio2').style.display = 'block';
+//    document.getElementById('btn2').style.display = 'none';
+// }
+
+// function ocultarPremio(){
+//     document.getElementById('premio2').style.display = 'none';
+//     document.getElementById('btn2').style.display = 'block';
+     
+// }
+
+// function mostrarPremio(){
+//     document.getElementById('premio3').style.display = 'block';
+//    document.getElementById('btn3').style.display = 'none';
+// }
+
+// function ocultarPremio(){
+//     document.getElementById('premio3').style.display = 'none';
+//     document.getElementById('btn3').style.display = 'block';
+     
+// }
+
+
+// function mostrarPremio(){
+//     document.getElementById('premio4').style.display = 'block';
+//    document.getElementById('btn4').style.display = 'none';
+// }
+
+// function ocultarPremio(){
+//     document.getElementById('premio4').style.display = 'none';
+//     document.getElementById('btn4').style.display = 'block';
+     
+// }
+
+// function mostrarPremio(){
+//     document.getElementById('premio9').style.display = 'block';
+//    document.getElementById('btn5').style.display = 'none';
+// }
+
+// function ocultarPremio(){
+//     document.getElementById('premio').style.display = 'none';
+//     document.getElementById('btn5').style.display = 'block';
+     
+// }
+
+
+// function mostrarPremio(){
+//     document.getElementById('premio').style.display = 'block';
+//    document.getElementById('btn6').style.display = 'none';
+// }
+
+// function ocultarPremio(){
+//     document.getElementById('premio').style.display = 'none';
+//     document.getElementById('btn6').style.display = 'block';
+     
+// }
+
+// function mostrarPremio(){
+//     document.getElementById('premio').style.display = 'block';
+//    document.getElementById('btn7').style.display = 'none';
+// }
+
+// function ocultarPremio(){
+//     document.getElementById('premio').style.display = 'none';
+//     document.getElementById('btn7').style.display = 'block';
+     
+// }
+
+// function mostrarPremio(){
+//     document.getElementById('premio').style.display = 'block';
+//    document.getElementById('btn8').style.display = 'none';
+// }
+
+// function ocultarPremio(){
+//     document.getElementById('premio').style.display = 'none';
+//     document.getElementById('btn8').style.display = 'block';
+     
+// }
+
+// function mostrarPremio(){
+//     document.getElementById('premio').style.display = 'block';
+//    document.getElementById('btn9').style.display = 'none';
+// }
+
+// function ocultarPremio(){
+//     document.getElementById('premio').style.display = 'none';
+//     document.getElementById('btn9').style.display = 'block';
+     
+// }
